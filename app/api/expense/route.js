ffdb6f4a19d73b515301d2ca2071316b8a73cc94
dj21-vicky21 <djvicky141@gmail.com>
2024-03-17 17:56:55 +0530

    add expense api , function created

---
+++ app/api/expense/route.js
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+import prisma from "../auth/utils/db";
+import { getToken } from "next-auth/jwt";
+import { addExpenseSchemaServer } from "@/schema/schema";
+import { z } from "zod";
+
+const createExpense = async (payload, id) => {
+    try {
+        const { description, amount, date, currency } = payload
+
+        const data = {
+            title: description,
+            amount,
+            expense_date: date,
+            currency_code: currency,
+            userId: id
+        }
+        const addExpense = await prisma.expenses.create({
+            data
+        })
+        // console.log("addExpense", addExpense);
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const auth = async (req) => {
+    const token = await getToken({ req })
+    if (!token) return NextResponse.json({ Error: "Unauthorized!" }, { status: 401 })
+    return token
+}
+
+export async function POST(req, res) {
+    try {
+        const token = await auth(req)
+
+        const data = await req.json()
+        const validation = await addExpenseSchemaServer.parse(data);
+        const userId = token.sub
+
+        createExpense(data, userId)
+
+        return NextResponse.json({ Message: "Successfully added your record!" }, { status: 201 })
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            // Handle validation errors
+            return NextResponse.json({ error: "Invalid request data", details: error.errors }, { status: 400 })
+        }
+        // Handle other errors
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 })
+    }
+}
+
