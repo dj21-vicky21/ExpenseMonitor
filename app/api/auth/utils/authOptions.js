4adccca45d625cf2341785bd22d6746fbf3190f2
dj21-vicky21 <djvicky141@gmail.com>
2024-09-02 23:50:22 +0530

    removed email provider

--- app/api/auth/utils/authOptions.js
+++ app/api/auth/utils/authOptions.js
@@ -1,4 +1,4 @@
-import EmailProvider from "next-auth/providers/email";
+// import EmailProvider from "next-auth/providers/email";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter"
@@ -16,17 +16,17 @@ export const authOptions = {
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
         }),
-        EmailProvider({
-            server: {
-                host: process.env.EMAIL_SERVER_HOST,
-                port: process.env.EMAIL_SERVER_PORT,
-                auth: {
-                    user: process.env.EMAIL_SERVER_USER,
-                    pass: process.env.EMAIL_SERVER_PASSWORD
-                }
-            },
-            from: process.env.EMAIL_FROM,
-        }),
+        // EmailProvider({
+        //     server: {
+        //         host: process.env.EMAIL_SERVER_HOST,
+        //         port: process.env.EMAIL_SERVER_PORT,
+        //         auth: {
+        //             user: process.env.EMAIL_SERVER_USER,
+        //             pass: process.env.EMAIL_SERVER_PASSWORD
+        //         }
+        //     },
+        //     from: process.env.EMAIL_FROM,
+        // }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
     session: {
