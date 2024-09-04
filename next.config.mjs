5c044493ea35c3666818ccdaa1e07ddb260e7663
dj21-vicky21 <djvicky141@gmail.com>
2024-09-04 14:07:43 +0530

    update json

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'avatars.githubusercontent.com',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
       },
+    ],
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    // other environment variables
+  }
 };
 
 export default nextConfig;
