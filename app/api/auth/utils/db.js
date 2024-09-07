5dcbe7734ccf6b3a198f9978aeef420acd90eaae
dj21-vicky21 <djvicky141@gmail.com>
2024-09-07 13:57:47 +0530

    db fix

--- app/api/auth/utils/db.js
+++ app/api/auth/utils/db.js
@@ -1,11 +1,20 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+let prisma;
 
-const prisma = global.prisma ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV === 'production') {
+  // In production, we create a new instance of PrismaClient for each request
+  prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  });
+} else {
+  // In development, we use a global variable to reuse the PrismaClient instance
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({
+      log: ['query', 'info', 'warn', 'error'],
+    });
+  }
+  prisma = global.prisma;
+}
 
 module.exports = prisma;
